af45c52152578ee4339bc16f4fc1d677bd495d86
n-stott <aeastrolabe@gmail.com>
2021-09-07 00:23:01 +0200

    update makefiles + regen javascript + fix disable move after win

--- make_js.sh
+++ make_js.sh
@@ -1,7 +1,7 @@
 cd cpp
 ~/git/emsdk/upstream/emscripten/em++ src/*.cpp -Iinclude -Ilib/include\
     -std=c++2a -O3 -march=native -DNDEBUG\
-    -o ../web/js/yokai-lib.js\
+    -o ../web/js/yokai/yokai-lib.js\
     -s SINGLE_FILE\
     -s WASM=0\
     -s EXPORTED_FUNCTIONS='["_validAction", "_playAction", "_searchBestMove", "_board", "_reserve0", "_reserve1"]'\
